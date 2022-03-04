f48da33ec080afc0658ecc563daf2bedcde67bd2
Aladdin Koraiem <alaadahmed@gmail.com>
2022-03-04 07:53:09 +0200

    initial commit

---
+++ app/routes/blog.tsx
@@ -0,0 +1,18 @@
+import { LinksFunction, Outlet } from "remix";
+import styles from "highlight.js/styles/github-dark-dimmed.css";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: styles },
+  ];
+};
+
+export default function Blog() {
+  return (
+    <div className="flex justify-center py-10">
+      <div className="prose lg:prose-xl">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="p-10">
+      <h1 className="text-xl font-bold text-slate-800">My Blog</h1>
+    </div>
+  );
+}
